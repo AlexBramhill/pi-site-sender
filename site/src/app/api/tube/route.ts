05ca8870c4b1a60e53aa674cb3361d467868373b
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-04-29 22:47:31 +0100

    fix env var issue

--- site/src/app/api/tube/route.ts
+++ site/src/app/api/tube/route.ts
@@ -1,19 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getLineStatus } from "./tube";
+import { config } from "@/app/config/config";
 
-export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
-  const lineId = searchParams.get("lineId");
-
-  if (!lineId) {
-    return NextResponse.json(
-      { error: "Missing lineId parameter" },
-      { status: 400 }
-    );
-  }
-
+export async function GET() {
   try {
-    const status = await getLineStatus(lineId);
+    const status = await getLineStatus(config.HOME_TUBE_LINE_NAME);
     return NextResponse.json(status);
   } catch (error) {
     console.error("Error fetching line status:", error);
