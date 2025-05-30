db1cda56ce6af87dce081248ead5dcf94f5e65f8
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-05-30 01:13:51 +0100

    add tailwind

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -8,6 +8,7 @@ export default async function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
         <PicoGrid>
           <WeatherImage />
           <DigitalClock />
