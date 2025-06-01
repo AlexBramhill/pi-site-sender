9d93db64d1dda26f5663ad40cb1a4c10fe942201
Alex Bramhill <alex.bramhill@softwire.com>
2025-06-01 14:53:57 +0100

    update docker ignore

--- backend-orchestrator/src/clients/photo.ts
+++ backend-orchestrator/src/clients/photo.ts
@@ -5,12 +5,12 @@ import { PhotoQuery } from "../schemas/photo-query-schema.js";
 export const get = async (photoPath: string, params: PhotoQuery): Promise<any> => {
     const query = toQueryParamString(params);
 
+    // TODO: config out this hard coded port
     const response = await fetch(`http://photo:4000${photoPath}?${query}`, { method: 'GET' });
 
     if (!response.ok) {
         throw new Error(`Failed to fetch photo: ${response.statusText}`);
     }
 
-    const data = await response.arrayBuffer();
-    return data;
+    return response.arrayBuffer();
 };
