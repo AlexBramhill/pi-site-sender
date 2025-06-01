34459c143e36d4ab13447ed44e5d50b0d0e098f1
Alex Bramhill <alex.bramhill@softwire.com>
2025-06-01 15:21:09 +0100

    remove unused comment

--- photo/src/schemas/query-schema.ts
+++ photo/src/schemas/query-schema.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-// This is a work around due to how express seems to handle empty query params as an empty object and not null
 // There is also some further fun around the typing of these query params
 export const ScreenshotQuerySchema = z
   .object({
