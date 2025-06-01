71fdf088c84a41aa19447373025bacf6836259a1
Alex Bramhill <alex.bramhill@softwire.com>
2025-06-01 11:41:05 +0100

    wip adding of backend orchestrator

--- site/src/app/config/config.ts
+++ site/src/app/config/config.ts
@@ -6,10 +6,6 @@ dotenv.config();
 const configSchema = z.object({
   TFL_API_KEY: z.coerce.string(),
   HOME_TUBE_LINE_NAME: z.coerce.string(),
-  HOME_STATION_NAPTAN_ID: z.coerce.string(),
-  HOME_STATION_NAME: z.coerce.string(),
-  INBOUND_CARDINAL_DIRECTION: z.coerce.string(),
-  OUTBOUND_CARDINAL_DIRECTION: z.coerce.string(),
   REDIS_URL: z.coerce.string().default("redis://redis:6379"),
 });
 
