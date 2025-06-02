d6acd30d3a8f423f23135a70db0d3be3840bbc9f
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-02 22:26:29 +0100

    split screenshot and site into distinct bundles

--- dev.sh
+++ dev.sh
@@ -1,1 +1,6 @@
-docker compose --file docker-compose.dev.yml up --build
+#!/bin/bash
+(docker network inspect screenshot-network >/dev/null 2>&1 || docker network create screenshot-network) &
+(cd screenshot && bash dev.sh) &
+cd "$(dirname "$0")" &&
+(cd site && bash dev.sh) &
+wait
--- prod.sh
+++ prod.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+(docker network inspect screenshot-network >/dev/null 2>&1 || docker network create screenshot-network) &
 docker compose --file docker-compose.prod.yml up --build
