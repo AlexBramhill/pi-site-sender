2fcdf2a96d95de761b02044e14e0f59d387e62a9
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-04 01:58:31 +0100

    fix rotation

--- screenshot/backend-orchestrator/src/schemas/screenshot-rotation-schema.ts
+++ screenshot/backend-orchestrator/src/schemas/screenshot-rotation-schema.ts
@@ -1,10 +1,9 @@
 import { z } from "zod";
 
-export const ScreenshotRotationSchema = z.union([
-  z.literal(0),
-  z.literal(90),
-  z.literal(180),
-  z.literal(270),
-]);
+export const ScreenshotRotationSchema = z.coerce
+  .number()
+  .refine((val) => [0, 90, 180, 270].includes(val), {
+    message: "Rotation must be one of 0, 90, 180, or 270",
+  });
 
 export type ScreenshotRotation = z.infer<typeof ScreenshotRotationSchema>;
--- screenshot/backend-orchestrator/src/transformers/backend-orchestrator-query-to-screenshot-query.ts
+++ screenshot/backend-orchestrator/src/transformers/backend-orchestrator-query-to-screenshot-query.ts
@@ -5,12 +5,14 @@ import { ScreenshotQuerySchema } from "../schemas/screenshot-query-schema.js";
 export const transformToScreenshotQuery = (
   originalQuery: BackendOrchestratorQuery
 ): ScreenshotQuery => {
-  if (originalQuery.rotation % 90 !== 0) {
+  if ((originalQuery.rotation / 90) % 2 !== 0) {
+    console.log("switching width and height due to non-90 degree rotation");
     return ScreenshotQuerySchema.parse({
       width: originalQuery.height,
       height: originalQuery.width,
     });
   }
+  console.log("no rotation, keeping original dimensions");
   return ScreenshotQuerySchema.parse({
     width: originalQuery.width,
     height: originalQuery.height,
