d29f375d4e39e7b808c97fff33aeb40db1983dff
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-10 01:11:01 +0100

    remove console log

--- site/src/app/client/bbc-rss.ts
+++ site/src/app/client/bbc-rss.ts
@@ -11,23 +11,6 @@ export const getBBCHeadlines = async (): Promise<BbcRssClientResponse> => {
   const feed = await parser.parseURL(
     "https://feeds.bbci.co.uk/news/world/rss.xml"
   );
-  console.log("-------------------------------------");
-  console.log("-------------------------------------");
-  console.log("-------------------------------------");
-
-  console.dir({ feed }, { depth: null, colors: true });
-  console.log("-------------------------------------");
-  const result = BbcRssClientResponseSchema.parse(feed.items);
-  console.dir(
-    { result },
-    {
-      depth: null,
-      colors: true,
-    }
-  );
-  console.log("-------------------------------------");
-  console.log("-------------------------------------");
-  console.log("-------------------------------------");
 
   return BbcRssClientResponseSchema.parse(feed.items);
 };
