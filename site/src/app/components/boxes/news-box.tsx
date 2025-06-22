83ff7a212b8884e56b0afed14cab734b2d0fccd2
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-23 00:17:26 +0100

    revert newsbox size

--- site/src/app/components/boxes/news-box.tsx
+++ site/src/app/components/boxes/news-box.tsx
@@ -7,7 +7,7 @@ export const newsfeedBoxConfig: BoxConfig = {
   key: "newsfeed",
   minCols: 2,
   minRows: 1,
-  preferredCols: 2,
+  preferredCols: 3,
   preferredRows: 1,
   content: newsfeedContent,
 };
