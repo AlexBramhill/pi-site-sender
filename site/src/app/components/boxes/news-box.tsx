4d236cc4a56da6c805e2f6feaf705043b38ff070
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-06-23 00:17:04 +0100

    remove percent chagne

--- site/src/app/components/boxes/news-box.tsx
+++ site/src/app/components/boxes/news-box.tsx
@@ -7,7 +7,7 @@ export const newsfeedBoxConfig: BoxConfig = {
   key: "newsfeed",
   minCols: 2,
   minRows: 1,
-  preferredCols: 3,
+  preferredCols: 2,
   preferredRows: 1,
   content: newsfeedContent,
 };
