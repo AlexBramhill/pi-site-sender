72059ed86d150fe87c1b9857c07d93900105b9c5
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-07-08 00:34:58 +0100

    remove pico to submodule

--- pico/config.py
+++
@@ -1,5 +0,0 @@
-from src.enums.image_types import IMAGE_TYPE
-from src.display.configs.display_configs import DISPLAY_CONFIG
-
-DISPLAY_TO_USE = DISPLAY_CONFIG.WAVESHARE_3IN7_EINK
-IMAGE_FORMAT_TO_USE = IMAGE_TYPE.BMP_RAW
